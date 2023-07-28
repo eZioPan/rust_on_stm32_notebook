8811b8a39f684e6c0e774ad100be23336f0d1f9c
eZio Pan <eziopan@qq.com>
2023-07-28 21:31:09 +0800

    use pac::Peripherals to access SysTick, and ...
    use SysTick with "exception" handle

--- s10_systick/src/bin/s10c01_systick_basic_setup.rs
+++ s10_systick/src/bin/s10c01_systick_basic_setup.rs
@@ -13,57 +13,65 @@
 use panic_rtt_target as _;
 use rtt_target::{rprint, rtt_init_print};
 
-use stm32f4xx_hal::pac::{CorePeripherals, Peripherals};
-
-#[allow(unused)]
-const ENABLE_OFFSET: u8 = 0;
-#[allow(unused)]
-const TICKINTL_OFFSET: u8 = 1;
-#[allow(unused)]
-const CLKSOOURCE_OFFSET: u8 = 2;
-#[allow(unused)]
-const COUNTFLAG_OFFSET: u8 = 16;
+use stm32f4xx_hal::pac;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    let cp = CorePeripherals::take().expect("Cannot take Cortex peripherals");
-    let dp = Peripherals::take().expect("Cannot take device peripherals");
+    let dp = pac::Peripherals::take().expect("Cannot take device peripherals");
 
+    // 使用外部晶振，获得 8 MHz 时钟
     dp.RCC.cr.modify(|_, w| w.hseon().on());
     while dp.RCC.cr.read().hserdy().is_not_ready() {}
     dp.RCC.cfgr.modify(|_, w| w.sw().hse());
     while !dp.RCC.cfgr.read().sws().is_hse() {}
 
-    let systick = &cp.SYST;
+    // 虽然 STK 是 Cortex 核心的部件，但 stm32f4 这个 crate 也将其抽象到了 pac::Peripherals 中
+    // 注意，pac::CorePeripherals 依旧可以访问这个寄存器，我们这里会选择 pac::Peripherals，
+    // 是因为 pac::Peripherals 对 SysTick 的寄存器的字段做了解析，更容易操作
+    let systick = &dp.STK;
 
-    unsafe {
-        // HSE 8 MHz 输入，被 8 分频之后，剩下 1 MHz
-        // 向 RVR 写入 999_999，可以以 1 次每秒的速率让系统时钟重载
-        // RVR: Reload Value Register
-        systick.rvr.write(1_000_000 - 1);
+    // 依照 Cortex-M4 programming manual 的 4.5.5 SysTick design hints and tips 的说明
+    // SysTick 是有正确的初始化流程的
+    // 顺序为
+    // 1. 编写 STK_LOAD 寄存器的值
+    // 2. 清理 STK_VAL 寄存器的值
+    // 3. 编写 STK_CTRL 寄存器的值
 
-        systick.csr.modify(|w| set_bit(w, ENABLE_OFFSET));
-    };
+    // 将重载寄存器设置为 999_999，配合上下面的 AHB/8，就能获得 1s 一个下溢出触发的效果
+    systick
+        .load
+        .modify(|_, w| unsafe { w.reload().bits(999_999) });
 
-    let mut counter = 0;
+    // 这里我们清理了一下 STK_VAL 的值
+    // 虽然这个操作看起来非常没有意义，但实际上，如果我们不清理（准确来说是不写一下）这个寄存器
+    // SysTick 是不会运行的
+    systick.val.reset();
 
+    systick.ctrl.modify(|_, w| {
+        // 时钟源选择 AHB/8，结合 HSE，获得 1 MHz 的频率
+        w.clksource().bit(false);
+        // 最后我们开启计数器
+        w.enable().set_bit();
+        w
+    });
+
+    let mut cnt = 0;
+
+    // 下面展示的，是一个简易的秒计数器
     loop {
-        // 实际上系统时钟常见的功能是 delay，而非触发中断
-        if systick.csr.read() >> COUNTFLAG_OFFSET & 1 == 1 {
-            rprint!("\x1b[2K\r{}", counter);
-            counter += 1;
-        }
+        // 这里我们监测 STK_CTRL 的 CountFlag 位，当该位置 1 时，表示 SysTick 至少已经发生了一次下溢出
+        // 若我们监测到该值不为 0，就一直空循环
+        // 这行代码，类似于 delay 函数了，一直阻塞核心，直到计数器溢出
+        while !systick.ctrl.read().countflag().bit() {}
+
+        // 当我们检测到 SysTick 下溢出之后，首先需要清理 CountFlag 位
+        systick.ctrl.modify(|_, w| w.countflag().clear_bit());
+
+        cnt += 1;
+
+        // 然后我们可以打印一下计数器的值
+        rprint!("\x1b[2K\r{}", cnt);
     }
-}
-
-#[allow(dead_code)]
-fn set_bit(value: u32, offset: u8) -> u32 {
-    value | (1 << offset)
-}
-
-#[allow(dead_code)]
-fn clear_bit(value: u32, offset: u8) -> u32 {
-    value & !(1 << offset)
 }
---
+++ s10_systick/src/bin/s10c02_systick_exp.rs
@@ -0,0 +1,62 @@
+//! SysTick 的“中断”用法
+//!
+//! 注意到我们的“中断”打了引号，这是因为 SysTick 属于核心，它触发实际上是“异常（exception）”而非“中断（interrupt）”
+//! 这里有两个注意点
+//! 1. SysTick 抛出异常不需要经过 NVIC，只需要在 SysTick 自己的寄存器里启用中断（TickInt）即可
+//! 2. SysTick 的 Handle 的标记是 `#[exception]` 而非 `#[interrupt]`
+
+#![no_std]
+#![no_main]
+
+use core::cell::Cell;
+
+use cortex_m::interrupt::Mutex;
+use cortex_m_rt::exception;
+use panic_rtt_target as _;
+use rtt_target::{rprint, rtt_init_print};
+
+use stm32f4xx_hal::pac;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let dp = pac::Peripherals::take().expect("Cannot take device peripherals");
+
+    dp.RCC.cr.modify(|_, w| w.hseon().on());
+    while dp.RCC.cr.read().hserdy().is_not_ready() {}
+    dp.RCC.cfgr.modify(|_, w| w.sw().hse());
+    while !dp.RCC.cfgr.read().sws().is_hse() {}
+
+    let systick = &dp.STK;
+
+    systick
+        .load
+        .modify(|_, w| unsafe { w.reload().bits(999_999) });
+
+    systick.val.reset();
+
+    systick.ctrl.modify(|_, w| {
+        w.clksource().bit(false);
+        // 打开 SysTick 下溢出的中断
+        w.tickint().bit(true);
+        w.enable().set_bit();
+        w
+    });
+
+    loop {}
+}
+
+static G_CNT: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+
+#[exception]
+fn SysTick() {
+    cortex_m::interrupt::free(|cs| {
+        // 实际上我们没有必要清理 CountFlag，因为异常的抛出不依赖于这标志位
+        let cnt_handle = G_CNT.borrow(cs);
+        let mut cnt = cnt_handle.get();
+        cnt += 1;
+        rprint!("\x1b[2K\r{}", cnt);
+        cnt_handle.set(cnt);
+    });
+}
